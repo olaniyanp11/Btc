d2a625bc32ae7455814343c0d1fa4ae1eeee5b7d
Olaniyan pelumi Israel <olaniyanp11@gmail.com>
2025-10-17 22:58:20 +0100

    adjusted imported components in page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,36 @@
 import Image from "next/image";
+import Section from "../components/Section";
+import Nav from "../components/Nav";
+import HorizontalScroll from "../components/HorizontalScroll";
+import TwoColumnSection from "../components/TwoColumnSection"
+import Features from "../components/Features";
+import InvestmentPlan from "../components/InvestmentPlan"
+import VideoSection from "@/components/VideoSection";
+import STwoColumnSection from "@/components/STwoColumnSection"
+import PayoutHistory from "@/components/PayoutHistory"
+import Footer from "@/components/Footer"
+import BitcoinChart from "@/components/BitCoinChart";
+export default function Home() {
+  return (
+    <>
+      <div className="fixed bottom-3 left-3">
+        <Image src="/images/wa.png" alt="whatsapp" width={70}  height={70}/>
+      </div>
+      <Nav />
+      <HorizontalScroll />
+      <Section />
+      <TwoColumnSection />
+      <Features />
+      <InvestmentPlan />
+      <VideoSection
+        videoUrl="https://youtu.be/3QH7ZTibV-Q"
+        title="How to Buy Bitcoin"
+        description="   "
+      />
+      <STwoColumnSection
+        bgImage="/images/bg.png"
+        sideImage="/images/eth-1.jpg"
+        title="Concise Explainer"
         bgImage="/images/bg.png"
         sideImage="/images/eth-1.jpg"
         title="Concise Explainer"
